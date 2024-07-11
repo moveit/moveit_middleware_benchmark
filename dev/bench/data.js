31ce333dbc2f87a7b1b71ae35a7227a3925a5dc0
github-action-benchmark <github@users.noreply.github.com>
2024-07-11 22:36:45 +0000

    add Movet Middleware Benchmark Project Perception Pipeline Benchmark (googlecpp) benchmark result for a1ec074a5c71b052e7f0833871457cca811966a5

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1720737405593,
+  "repoUrl": "https://github.com/CihatAltiparmak/moveit_middleware_benchmark",
+  "entries": {
+    "Movet Middleware Benchmark Project Perception Pipeline Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "cihataltiparmak1@gmail.com",
+            "name": "CihatAltiparmak",
+            "username": "CihatAltiparmak"
+          },
+          "committer": {
+            "email": "cihataltiparmak1@gmail.com",
+            "name": "CihatAltiparmak",
+            "username": "CihatAltiparmak"
+          },
+          "distinct": true,
+          "id": "a1ec074a5c71b052e7f0833871457cca811966a5",
+          "message": "Fixed run_benchmarks.yml github action for fixing the problem to push results",
+          "timestamp": "2024-07-12T01:29:43+03:00",
+          "tree_id": "a5fe0a5e192934421426f8aa5a5948cf6dc29c96",
+          "url": "https://github.com/CihatAltiparmak/moveit_middleware_benchmark/commit/a1ec074a5c71b052e7f0833871457cca811966a5"
+        },
+        "date": 1720737405199,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 257076855860.00003,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1715919191 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
