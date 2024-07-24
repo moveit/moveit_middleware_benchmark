88ffc2cc86e06e1eb46bd4829967da0ff2faab9c
CihatAltiparmak <cihataltiparmak1@gmail.com>
2024-07-24 23:02:16 +0300

    Added scripts related to visualization, running benchmarks etc.

---
+++ middleware_configurations/rmw_zenoh/config_rmw_zenoh.sh
@@ -0,0 +1,5 @@
+echo "The configurations for rmw_zenoh_cpp is started!"
+# sudo sysctl -w "net.ipv4.tcp_rmem=4096 4096 4096"
+# sudo sysctl -w "net.ipv4.tcp_wmem=4096 4096 4096"
+# sudo sysctl -w "net.ipv4.tcp_mem=4096 4096 4096"
+echo "The configurations for rmw_zenoh_cpp is finished!"
