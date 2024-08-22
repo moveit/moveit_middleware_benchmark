cec6b08386d57539c680503fd2078d6771c67372
github-action-benchmark <github@users.noreply.github.com>
2024-08-22 16:32:38 +0000

    add Perception Pipeline Benchmark (googlecpp) benchmark result for 2d4a2469515123655e673fda9a1c85e3eb7e88bb

---
+++ rmw_cyclonedds/data.js
@@ -0,0 +1,92 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724344358444,
+  "repoUrl": "https://github.com/CihatAltiparmak/moveit_middleware_benchmark",
+  "entries": {
+    "Perception Pipeline Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "cihataltiparmak1@gmail.com",
+            "name": "CihatAltiparmak",
+            "username": "CihatAltiparmak"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2d4a2469515123655e673fda9a1c85e3eb7e88bb",
+          "message": "Removed unnecessary scores from perception pipeline benchmark (#28)",
+          "timestamp": "2024-08-22T11:07:32+03:00",
+          "tree_id": "aabadeb058a009dc9d25fb053091c443b61207ef",
+          "url": "https://github.com/CihatAltiparmak/moveit_middleware_benchmark/commit/2d4a2469515123655e673fda9a1c85e3eb7e88bb"
+        },
+        "date": 1724344358258,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 197106027231.0001,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1237875398 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 225190659040.00012,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1413290919 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 224042813814.00006,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1404077035.0000002 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 175238378960.99985,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1100694954.9999998 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 175440801112.00037,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1102173551 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 182452332214.99985,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1144371836.0000005 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline_mean",
+            "value": 196578502062.16666,
+            "unit": "ns/iter",
+            "extra": "iterations: 6\ncpu: 1233747282.3333333 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline_median",
+            "value": 189779179722.99997,
+            "unit": "ns/iter",
+            "extra": "iterations: 6\ncpu: 1191123617.0000002 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline_stddev",
+            "value": 23129955294.865925,
+            "unit": "ns/iter",
+            "extra": "iterations: 6\ncpu: 144407563.82060644 ns\nthreads: 1"
+          },
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline_cv",
+            "value": 0.1176626897256101,
+            "unit": "ns/iter",
+            "extra": "iterations: 6\ncpu: 0.11704792860616854 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
